310a9ca85daf12a4d8d87c0fa41874d454681db3
Jeison190495 <Jeison.aguirre.1995@hotmail.com>
2024-09-04 19:39:27 -0500

    Update parqueadero.py

--- parqueadero.py
+++ parqueadero.py
@@ -1,14 +1,59 @@
 import datetime
+#import cx_Oracle
+import psycopg2
+
+#conectar base dedatos oracle
+"""
+def conectar_oracle():
+    try:
+        connection = cx_Oracle.connect("SYSTEM", "Sistemas.2023", "localhost/1521")
+        print("Conectado a Oracle")
+        return connection
+    except cx_Oracle.DatabaseError as e:
+        print(f"Error al conectar a Oracle: {e}")
+"""
+# Conectar a la base de datos PostgreSQL
+def conectar_postgres():
+    try:
+        connection = psycopg2.connect(
+            dbname="nombre_db", 
+            user="usuario", 
+            password="contraseña", 
+            host="localhost", 
+            port="5432"
+        )
+        print("Conectado a PostgreSQL")
+        return connection
+    except psycopg2.DatabaseError as e:
+        print(f"Error al conectar a PostgreSQL: {e}")
+
+# Uso de las conexiones
+"""oracle_conn = conectar_oracle()"""
+postgres_conn = conectar_postgres()
+
+# No olvides cerrar las conexiones al finalizar
+"""if oracle_conn:
+    oracle_conn.close()"""
+if postgres_conn:
+    postgres_conn.close()
+
+
+
 
 print ('Bienvenido al parqueadero')
 
-hora=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+horaIngreso=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+horaSalida=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 def vehiculo():
     placa=input('Ingrese la placa del vehiculo: ')
     return placa
     
-    
+def precio():
+     tiempopagar = (horaSalida - horaIngreso).total_seconds() / 60
+     tvehiculo={'moto':50,
+               'carro':70,
+               'bicicleta':40
+               }
 def init():
     try:
         while True:
@@ -29,12 +74,12 @@ def init():
 
             if (opcion==1):
                 placa=vehiculo()
-                print(f"Ingresando {tvehiculo} de placa: {placa}, {hora}")
+                print(f"Ingresando {tvehiculo} de placa: {placa}, {horaIngreso}")
             
             elif (opcion==2):
                 print('Retirando vehiculo')
                 placa=vehiculo()
-                print(f"vehiculo de placa {placa} saliendo a {hora}")
+                print(f"vehiculo de placa {placa} saliendo a {horaSalida}")
             
             elif (opcion==3):
                 print('Esta seguro que desea salir?')
